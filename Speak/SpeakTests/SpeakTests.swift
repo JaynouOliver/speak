9800f0072ec394abebb13b4050927c4e7951e8ae
JaynouOliver <jyb2084.usa@gmail.com>
2025-04-23 12:11:53 +0530

    Initial commit: Basic UI implementation

---
+++ Speak/SpeakTests/SpeakTests.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakTests.swift
+//  SpeakTests
+//
+//  Created by Subhro on 4/23/25.
+//
+
+import Testing
+@testable import Speak
+
+struct SpeakTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
